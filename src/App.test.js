2a9492e73c56889e8e38697b5f1af7d15e89982c
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2022-08-12 22:11:55 +0530

    breaking down app.test syntax

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  render(<App />); // 'render' creates virtual DOM for argument(JSX)
+  // access V-DOM via 'screen' global object
+  const linkElement = screen.getByText(/learn react/i); // finding element by display text using 'screen.getByText'
+  expect(linkElement).toBeInTheDocument(); // Lastly, assertion!
 });
